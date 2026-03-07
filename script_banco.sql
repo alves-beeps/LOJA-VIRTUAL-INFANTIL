323d543c48e48912f0e684dfe549542c7f7c4a95
alves-beeps <rudsonrafael17031988@gmail.com>
2026-03-06 21:45:56 -0400

    Add files via upload

--- script_banco.sql
+++ script_banco.sql
@@ -1,32 +1,9 @@
-PROJETO: BANCO DE DADOS - LOJA VIRTUAL INFANTIL
-  DISCIPLINA: BANCO DE DADOS - AEMS
-  AUTOR: RUDSON RAFAEL ALVES RIBEIRO
-1. DICIONÁRIO DE DADOS
-  
-  TABELA: Clientes
-  - id_cliente (INT): Chave Primária, incremento automático.
-  - nome_completo (VARCHAR 150): Nome do responsável pela compra.
-  - cpf (CHAR 11): Cadastro de Pessoa Física (apenas números, único).
-  - email (VARCHAR 100): E-mail para contato e login (único).
-  - telefone (VARCHAR 20): Telefone com DDD.
-  
-  TABELA: Produtos
-  - id_produto (INT): Chave Primária, incremento automático.
-  - nome (VARCHAR 100): Nome comercial do produto.
-  - preco (DECIMAL 10,2): Preço de venda com duas casas decimais.
-  - estoque_qtd (INT): Quantidade atual no inventário.
-  - categoria (VARCHAR 50): Segmentação (Ex: Brinquedos, Calçados).
-  
-  2. LÓGICA DO MODELO (MER/DER)
-  O sistema utiliza relacionamentos para garantir a integridade:
-  - Um CLIENTE pode realizar vários PEDIDOS (1:N).
-  - Um PEDIDO contém vários PRODUTOS através da tabela ITENS_PEDIDO (N:N).
-
--- CRIAÇÃO DO BANCO DE DADOS
-CREATE DATABASE IF NOT EXISTS LojaInfantil;
+SQL
+-- Criação do Banco de Dados
+CREATE DATABASE LojaInfantil;
 USE LojaInfantil;
 
--- TABELA DE CLIENTES
+-- Tabela de Clientes
 CREATE TABLE Clientes (
     id_cliente INT AUTO_INCREMENT PRIMARY KEY,
     nome_completo VARCHAR(150) NOT NULL,
@@ -36,7 +13,7 @@ CREATE TABLE Clientes (
     data_cadastro TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
--- TABELA DE PRODUTOS
+-- Tabela de Produtos
 CREATE TABLE Produtos (
     id_produto INT AUTO_INCREMENT PRIMARY KEY,
     nome VARCHAR(100) NOT NULL,
@@ -46,7 +23,7 @@ CREATE TABLE Produtos (
     categoria VARCHAR(50)
 );
 
--- TABELA DE PEDIDOS (Relaciona o pedido ao cliente)
+-- Tabela de Pedidos
 CREATE TABLE Pedidos (
     id_pedido INT AUTO_INCREMENT PRIMARY KEY,
     id_cliente INT,
@@ -56,7 +33,7 @@ CREATE TABLE Pedidos (
     FOREIGN KEY (id_cliente) REFERENCES Clientes(id_cliente)
 );
 
--- TABELA DE ITENS DO PEDIDO (Relaciona produtos aos pedidos)
+-- Tabela de Itens do Pedido (Relacionamento N:N)
 CREATE TABLE Itens_Pedido (
     id_item INT AUTO_INCREMENT PRIMARY KEY,
     id_pedido INT,
